d11fdb24254393af455552e02ed95ddb30cbc186
justenbphelps <phelps.justen@gmail.com>
2025-05-27 17:09:48 -0700

    update

--- track.js
+++ track.js
@@ -4,7 +4,7 @@ const nodemailer = require('nodemailer');
 const crypto = require('crypto');
 const { URL } = require('url');
 
-const transporter = nodemailer.createTransporter({
+const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
     user: process.env.EMAIL_USER,
